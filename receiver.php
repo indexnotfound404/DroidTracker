104e86cc0db570e712f4eb3ce89bfa3193d9644e
The Linux Choice <34893261+thelinuxchoice@users.noreply.github.com>
2019-01-07 21:45:55 -0200

    Update receiver.php

--- receiver.php
+++ receiver.php
@@ -15,14 +15,14 @@ $http_client_ip       = $_SERVER['HTTP_CLIENT_IP'];
 $http_x_forwarded_for = $_SERVER['HTTP_X_FORWARDED_FOR'];
 $remote_addr          = $_SERVER['REMOTE_ADDR'];
  
-/* VERIFICO SE O IP REALMENTE EXISTE NA INTERNET */
+
 if(!empty($http_client_ip)){
     $ip = $http_client_ip;
-    /* VERIFICO SE O ACESSO PARTIU DE UM SERVIDOR PROXY */
+    
 } elseif(!empty($http_x_forwarded_for)){
     $ip = $http_x_forwarded_for;
 } else {
-    /* CASO EU NÃO ENCONTRE NAS DUAS OUTRAS MANEIRAS, RECUPERO DA FORMA TRADICIONAL */
+    
     $ip = $remote_addr."\n";
 }
  
